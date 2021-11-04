fe015cac5ea832164e532e1bab5265934d30def3
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 08:28:59 +0700

    #7 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-sample
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/4/2021
+ * Time: 8:28 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+}
