fc6999f2bc0f014bda2722279aea62281a2f1e49
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-05 18:58:47 +0700

    #27 - Add getAll in ProductController

--- src/main/java/com/hendisantika/controller/ProductController.java
+++ src/main/java/com/hendisantika/controller/ProductController.java
@@ -1,9 +1,17 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Product;
 import com.hendisantika.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +29,12 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Product>> getAll() {
+        List<Product> products = productService.getAll();
+        return ResponseEntity.ok(products);
+    }
 }
