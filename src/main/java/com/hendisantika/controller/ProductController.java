5f522dcd1705a3258c6ca7a68867227da6ed0157
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-05 18:59:53 +0700

    #28 - Add showProducts in ProductController

--- src/main/java/com/hendisantika/controller/ProductController.java
+++ src/main/java/com/hendisantika/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -36,5 +37,12 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAll() {
         List<Product> products = productService.getAll();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/page")
+    public ModelAndView showProducts() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("products");
+        return modelAndView;
     }
 }
